6866590d159b40dc325f4fbf6fd958687a9bc385
Satyawan Augustinus <dev.satyawan@gmail.com>
2020-02-05 22:43:03 +0700

    membuat instance produk3

--- produk.php
+++ produk.php
@@ -8,11 +8,19 @@ class Produk
         $harga = 0;
 }
 
-$produk1 = new Produk();
+/* $produk1 = new Produk();
 $produk1->judul = "Naruto";
 var_dump($produk1);
 
 $produk2 = new Produk();
 $produk2->judul = "Dragon Ball";
 $produk2->tambahProperty = "Hahahihi";
-var_dump($produk2);
+var_dump($produk2); */
+
+$produk3 = new Produk();
+$produk3->judul = "Wiro Sableng";
+$produk3->penulis = "Ken Ken";
+$produk3->penerbit = "Kompas Gramedia";
+$produk3->harga = 55000;
+//var_dump($produk3);
+echo "Komik: $produk3->judul, $produk3->penulis";
