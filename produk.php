5ef2566ed6bbbfc0eb0b5a0b52e39a8183b9243a
Satyawan Augustinus <dev.satyawan@gmail.com>
2020-02-05 21:29:21 +0700

    membuat instance produk dan mengganti propertinya

--- produk.php
+++ produk.php
@@ -9,5 +9,10 @@ class Produk
 }
 
 $produk1 = new Produk();
-
+$produk1->judul = "Naruto";
 var_dump($produk1);
+
+$produk2 = new Produk();
+$produk2->judul = "Dragon Ball";
+$produk2->tambahProperty = "Hahahihi";
+var_dump($produk2);
